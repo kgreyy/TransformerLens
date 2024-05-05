7ad561be635417fb2f49658c64456d98026b552d
Kyle Reynoso <57283823+kgreyy@users.noreply.github.com>
2024-05-06 01:41:32 +0800

    feat: add RMU zephyr model

--- transformer_lens/loading_from_pretrained.py
+++ transformer_lens/loading_from_pretrained.py
@@ -31,6 +31,7 @@ OFFICIAL_MODEL_NAMES = [
     "facebook/opt-30b",
     "facebook/opt-66b",
     "HuggingFaceH4/zephyr-7b-beta",
+    "cais/Zephyr_RMU",
     "EleutherAI/gpt-neo-125M",
     "EleutherAI/gpt-neo-1.3B",
     "EleutherAI/gpt-neo-2.7B",
